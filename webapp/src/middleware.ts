448d4afa40a175f103c920e53e7f9fa777a84187
OptionsBuffetDev <mcrafter147@gmail.com>
2024-08-08 11:22:56 +0530

    [Changes]

---
+++ webapp/src/middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const notPublicRoute = createRouteMatcher([
+    '/'
+])
+
+export default clerkMiddleware((auth, request) => {
+    if(notPublicRoute(request)) {
+        auth().protect()
+    }
+});
+
+export const config = {
+  matcher: [
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
+  ],
+};
